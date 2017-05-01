3b6fd103aadd81965f2609385e78cdbc3c67be0c
LemonTure <d228788@edu.rocwb.nl>
2017-05-01 16:53:01 +0200

    Added wedstrijdschema + teamLijst + topscorerLijst

--- Web/public/index.php
+++ Web/public/index.php
@@ -4,26 +4,66 @@
 
         <div>
             <?php
-
-            foreach ($players as $player){
-                echo "<ul>
-                        <li>{$player['first_name']}</li>
-                        <li>{$player['last_name']}</li>
-                        <li>{$player['team_id']}</li>
-                     </ul>";
-            }
+                
+                foreach ($matches as $match){
+                    
+                    $matchTeamsA = "SELECT * FROM tbl_teams WHERE id='{$match['team_id_a']}'";
+                    $matchTeamsA = $db_conn->query($matchTeamsA);
+                    foreach ($matchTeamsA as $matchTeamA) {
+                        echo "<ul>
+                                    <li>{$matchTeamA['name']}</li>
+                              </ul>";
+                    }
+                    
+                    $matchTeamsB = "SELECT * FROM tbl_teams WHERE id='{$match['team_id_b']}'";
+                    $matchTeamsB = $db_conn->query($matchTeamsB);
+                    foreach ($matchTeamsB as $matchTeamB){
+                        echo "<ul>
+                                <li>{$matchTeamB['name']}</li>
+                              </ul>";
+                    }
+    
+                    echo "<ul>
+                                <li>{$match['start_time']}</li>
+                            </ul>";
+                }
             ?>
         </div>
         <div class="">
             <?php
 
             foreach ($teams as $team){
-
+                echo "<ul>
+                        <li>{$team['name']}</li>
+                       </ul>";
+                $teamPlayers = "SELECT * FROM tbl_players WHERE team_id='{$team['id']}'";
+                $teamPlayers = $db_conn->query($teamPlayers);
+                foreach ($teamPlayers as $teamPlayer){
+                    echo
+                    "<ul>
+                        <li>{$teamPlayer['first_name']}</li>
+                    </ul>
+                    ";
+                }
             }
             ?>
         </div>
-
-
+        
+        <div class="">
+            <?php
+                $playerGoals = "SELECT * FROM tbl_players ORDER BY goals DESC";
+                $playerGoals = $db_conn->query($playerGoals);
+                $playerGoals = $playerGoals->fetchAll(PDO::FETCH_ASSOC);
+                foreach ($playerGoals as $playerGoal){
+                    echo "<ul>
+                             <li>{$playerGoal['first_name']}</li>
+                             <li>{$playerGoal['last_name']}</li>
+                             <li>{$playerGoal['goals']}</li>
+                        </ul>";
+                }
+            
+            ?>
+        </div>
     </div>
 
 <?php require(realpath(__DIR__) . '/templates/footer.php');
