82339adbcddb188a3394a20639705e4d29da932c
Unknown <d233407@edu.rocwb.nl>
2017-05-01 14:00:25 +0200

    start website

--- Web/public/index.php
+++ Web/public/index.php
@@ -2,7 +2,26 @@
 
     <div class="main-content">
 
+        <div>
+            <?php
 
+            foreach ($players as $player){
+                echo "<ul>
+                        <li>{$player['first_name']}</li>
+                        <li>{$player['last_name']}</li>
+                        <li>{$player['team_id']}</li>
+                     </ul>";
+            }
+            ?>
+        </div>
+        <div class="">
+            <?php
+
+            foreach ($teams as $team){
+
+            }
+            ?>
+        </div>
 
 
     </div>
